e6e15ecf607f53bd949d9e70822b810ea128372e
Rayan Junio <rayanjunior234@gmail.com>
2025-06-11 14:55:05 -0300

    feat: add new dto for request login

---
+++ src/main/java/com/library/dto/request/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package com.library.dto.request;
+
+public class LoginRequestDTO {
+  private String email;
+  private String password;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
