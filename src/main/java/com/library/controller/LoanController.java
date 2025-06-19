66f3f4127a752a6136950196ab1c76a425f082dc
Rayan Junio <rayanjunior234@gmail.com>
2025-06-18 22:33:20 -0300

    feat(LoanController): add endpoints to create and finish a loan

---
+++ src/main/java/com/library/controller/LoanController.java
@@ -0,0 +1,43 @@
+package com.library.controller;
+
+import com.library.bo.LoanBO;
+import com.library.dto.request.loan.LoanRequestDTO;
+import com.library.dto.response.LoanResponseDTO;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+@Path("loan")
+public class LoanController {
+
+  @Inject
+  LoanBO loanBO;
+
+  @POST
+  @RolesAllowed({ "ADMIN" })
+  public Response createLoan(@Valid LoanRequestDTO loanRequestDTO) {
+    try {
+      LoanResponseDTO response = loanBO.createLoan(loanRequestDTO);
+      return Response.status(Response.Status.CREATED).entity(response).build();
+    } catch (IllegalArgumentException e) {
+      return Response.status(Response.Status.BAD_REQUEST).entity(e).build();
+    }
+  }
+
+  @PUT
+  @Path("{loanId}")
+  @RolesAllowed( { "ADMIN" })
+  public Response finishLoan(@PathParam("loanId") int loanId) {
+    try {
+      LoanResponseDTO response = loanBO.finishLoan(loanId);
+      return Response.status(Response.Status.OK).entity(response).build();
+    } catch (IllegalArgumentException e) {
+      return Response.status(Response.Status.BAD_REQUEST).entity(e).build();
+    }
+  }
+}
