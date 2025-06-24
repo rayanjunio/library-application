36401659e42d8f4fd467df2c79e1f5471f576303
Rayan Junio <rayanjunior234@gmail.com>
2025-06-24 09:14:06 -0300

    feat(JwtContext): add method to extract user id from authenticated jwt

---
+++ src/main/java/com/library/security/JwtContext.java
@@ -0,0 +1,18 @@
+package com.library.security;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.json.JsonNumber;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+@RequestScoped
+public class JwtContext {
+
+  @Inject
+  JsonWebToken jwt;
+
+  public long getUserId() {
+    JsonNumber jsonNumber = jwt.getClaim("userId");
+    return jsonNumber.longValue();
+  }
+}
