1e88a276e8c1b9134c9fc8ade7757600d160b55b
Rayan Junio <rayanjunior234@gmail.com>
2025-07-17 20:04:23 -0300

    feat(Templates): add qute templates to render HTML files

---
+++ backend/src/main/java/com/library/templates/Templates.java
@@ -0,0 +1,25 @@
+package com.library.templates;
+
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+
+@CheckedTemplate
+public class Templates {
+
+  public static native TemplateInstance index();
+  public static native TemplateInstance register();
+
+
+  public static native TemplateInstance dashboard_admin();
+  public static native TemplateInstance dashboard_user();
+
+  public static native TemplateInstance emprestimos_admin();
+  public static native TemplateInstance emprestimos_user();
+
+  public static native TemplateInstance livros_admin();
+
+  public static native TemplateInstance livros_user();
+
+  public static native TemplateInstance usuarios_admin();
+
+}
