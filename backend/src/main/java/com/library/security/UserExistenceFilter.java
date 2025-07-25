08fd2f7a704d23d6509727943f0ec5f62d3e63eb
Rayan Junio <rayanjunior234@gmail.com>
2025-07-25 20:52:15 -0300

    refactor(UserExistenceFilter): return a page when request is aborted

--- backend/src/main/java/com/library/security/UserExistenceFilter.java
+++ backend/src/main/java/com/library/security/UserExistenceFilter.java
@@ -3,11 +3,13 @@ package com.library.security;
 import com.library.exception.type.BusinessException;
 import com.library.model.bo.UserBO;
 import com.library.service.AuthService;
+import com.library.templates.Templates;
 import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.NewCookie;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
@@ -43,7 +45,8 @@ public class UserExistenceFilter implements ContainerRequestFilter {
           requestContext.abortWith(
                   Response.status(Response.Status.UNAUTHORIZED)
                           .cookie(expiredCookie)
-                          .entity("Usuário inexistente.")
+                          .entity(Templates.unauthorized().render())
+                          .type(MediaType.TEXT_HTML)
                           .build()
           );
         }
